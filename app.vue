5591b4c8fe07beb1314ee3a444414696f00f3d63
Alex Sizov <alex_sizow@outlook.com>
2024-12-01 18:58:51 +0300

    First styles config

--- app.vue
+++ app.vue
@@ -1,6 +1,23 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <header>
+    <NuxtLink to="/">Главная</NuxtLink>
+    <NuxtLink to="/about">О нас</NuxtLink>
+    <NuxtLink to="/users">Пользователи</NuxtLink>
+    <NuxtLink to="/users/123">Пользователь 123</NuxtLink>
+  </header>
+
+  <NuxtPage />
 </template>
+
+<script setup>
+// Здесь можно добавить глобальную логику
+</script>
+
+<style>
+.nuxt-loading-indicator {
+  background: repeating-linear-gradient(to right,
+      #00dc82 0%,
+      #34cdfe 50%,
+      #0047e1 100%);
+}
+</style>
