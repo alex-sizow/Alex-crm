5812da4e60924f7132f07882ccd3248fa0263a0e
Alex Sizov <alex_sizow@outlook.com>
2024-12-01 19:17:52 +0300

    nuxt settings fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,39 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+// nuxt.config.ts
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }, 
+  devtools: { enabled: true },
   css: [
     '@/assets/styles/reset.css',
     '@/assets/styles/variables.css'
-  ]
+  ],
+
+  components: [
+    {
+      path: '~/components',
+      pathPrefix: false,
+    },
+    {
+      path: '~/components/ui',
+      prefix: 'Ui',
+      global: true
+    },
+    {
+      path: '~/components/common',
+      prefix: 'Common',
+      global: true
+    }
+  ],
+
+  typescript: {
+    strict: true,
+    typeCheck: false
+  },
+
+  vite: {
+    vue: {
+      script: {
+        defineModel: true,
+        propsDestructure: true
+      }
+    }
+  }
 })
